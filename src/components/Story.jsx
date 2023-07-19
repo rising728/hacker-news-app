6cfc19e734b9a3dd2f273df58c87712e0757d5bc
rising728 <vincethedev@gmail.com>
2023-07-20 01:56:12 +0800

    Created Story component, to render each news fetched from the API

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -1,11 +1,60 @@
-import React from "react";
+import React, { useCallback } from "react";
+import { useNavigate } from "react-router-dom";
+import PropTypes from "prop-types";
+import DateTime from "./DateTime";
 
-const Stories = () => {
+const Story = ({ story }) => {
+  const { id, by, title, descendants, kids, time, url } = story;
+  const navigate = useNavigate();
+
+  const showComments = useCallback(() => {
+    navigate(`/hacker-news/${id}/comments`, { state: { story } });
+  }, [navigate, id, story]);
+
   return (
-    <div>
-      <h1>Welcome to the Story Page!</h1>
+    <div className="story">
+      <div className="story-title">
+        <a href={url}>{title}</a>
+      </div>
+      <div className="story-info">
+        <span>
+          by <a href={`https://news.ycombinator.com/user?id=${by}`}>{by}</a>
+        </span>
+        |{" "}
+        <span>
+          <DateTime timestamp={time} />
+        </span>{" "}
+        |
+        {descendants > 0 ? (
+          <>
+            <span>
+              <a href={`https://news.ycombinator.com/item?id=${id}`}>
+                {`${descendants} comments`}
+              </a>
+            </span>
+            |{" "}
+            <span>
+              <button onClick={showComments}>Show Comments</button>
+            </span>
+          </>
+        ) : (
+          <span>No comments.</span>
+        )}
+      </div>
     </div>
   );
 };
 
-export default Stories;
+Story.propTypes = {
+  story: PropTypes.shape({
+    id: PropTypes.number,
+    by: PropTypes.string,
+    title: PropTypes.string,
+    descendants: PropTypes.number,
+    kids: PropTypes.array,
+    time: PropTypes.number,
+    url: PropTypes.string,
+  }).isRequired,
+};
+
+export default Story;
