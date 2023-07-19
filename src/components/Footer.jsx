a73f92c24af67777c6158122e79fd652b0926a31
rising728 <vincethedev@gmail.com>
2023-07-20 01:23:29 +0800

    Updated Loader and Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="p-6 text-center bg-gray-300">
-      <p>&copy; 2023. All rights reserved.</p>
+    <footer className="p-6 text-center">
+      <p>&copy; 2023. Hacker News API. All rights reserved.</p>
     </footer>
   );
 };
