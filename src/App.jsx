44253bc9c83eda203179668b9477eb24e8f93228
rising728 <vincethedev@gmail.com>
2023-07-19 23:57:09 +0800

    Some basic changes to App component

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="App flex flex-col min-h-screen justify-between">
       <Header />
-      <NoMatch />
+      <div>Home Page</div>
       <Footer />
     </div>
   );
