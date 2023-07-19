902ae293ac7009f48b3da67e396e734198a38dfd
rising728 <vincethedev@gmail.com>
2023-07-20 00:45:29 +0800

    Removed all code in App.css, Added navigation using Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,33 @@
 import React from "react";
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import "./App.css";
 
 import NoMatch from "./pages/NoMatch";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Stories from "./pages/Stories";
 
 function App() {
   return (
     <div className="App flex flex-col min-h-screen justify-between">
-      <Header />
-      <div>Home Page</div>
-      <Footer />
+      <Router>
+        <Header />
+        <div className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Navigate to="/top" />} />
+            <Route path="/top" element={<Stories />} />
+            <Route path="/new" element={<Stories />} />
+            <Route path="/best" element={<Stories />} />
+            <Route path="*" element={<NoMatch />} />
+          </Routes>
+        </div>
+        <Footer />
+      </Router>
     </div>
   );
 }
